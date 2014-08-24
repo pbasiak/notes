2b2badb7691d3e0b577bc4fccb95329f028da55e
pbasiak <pawel.basiak01@gmail.com>
2014-08-24 16:23:30 +0200

    Travis Test

--- tests/index.js
+++ tests/index.js
@@ -19,6 +19,51 @@ suite('MeteoNote Test', function() {
        });
   });
 
+  test('dodaj listę - nazwa dwuczlonowa - klient', function(done, client) {
+    client.eval(function() {
+        Lists.insert({
+          name: "testowa lista",
+        });
+    var lista = Lists.find({ name: 'testowa'}).fetch();
+    emit('lista', lista);
+    });
+
+    client.once('lista', function(lista) {
+         assert.equal(lista.length, 1);
+         done();
+       });
+  });
+
+  test('dodaj listę - pusta wartosc - klient', function(done, client) {
+    client.eval(function() {
+        Lists.insert({
+          name: "",
+        });
+    var lista = Lists.find({ name: 'testowa'}).fetch();
+    emit('lista', lista);
+    });
+
+    client.once('lista', function(lista) {
+         assert.equal(lista.length, 1);
+         done();
+       });
+  });
+
+  test('usunięcie listy - klient', function(done, client) {
+    client.eval(function() {
+        Lists.remove({
+          name: "testowa",
+        });
+    var lista = Lists.find({ name: 'testowa'}).fetch();
+    emit('lista', lista);
+    });
+
+    client.once('lista', function(lista) {
+         assert.equal(lista.length, 1);
+         done();
+       });
+  });
+
   
 
 });
