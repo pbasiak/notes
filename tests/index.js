4d4c0383eed4826a54376332198e45f505b83be4
pbasiak <pawel.basiak01@gmail.com>
2014-08-24 17:16:16 +0200

    Travis up

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,3 @@
-'use strict'
 
 var assert = require('assert');
 
@@ -24,12 +23,12 @@ suite('MeteoNote Test', function() {
         Lists.insert({
           name: "testowa lista",
         });
-    var lista = Lists.find({ name: 'testowa'}).fetch();
-    emit('lista', lista);
+    var lista2 = Lists.find({ name: 'testowa lista'}).fetch();
+    emit('lista2', lista2);
     });
 
-    client.once('lista', function(lista) {
-         assert.equal(lista.length, 1);
+    client.once('lista2', function(lista2) {
+         assert.equal(lista2.length, 1);
          done();
        });
   });
@@ -39,27 +38,30 @@ suite('MeteoNote Test', function() {
         Lists.insert({
           name: "",
         });
-    var lista = Lists.find({ name: 'testowa'}).fetch();
-    emit('lista', lista);
+    var lista3 = Lists.find({ name: ''}).fetch();
+    emit('lista3', lista3);
     });
 
-    client.once('lista', function(lista) {
-         assert.equal(lista.length, 1);
+    client.once('lista3', function(lista3) {
+         assert.equal(lista3.length, 1);
          done();
        });
   });
 
   test('usunięcie listy - klient', function(done, client) {
     client.eval(function() {
+    	Lists.insert({
+    		name: "testowaD",
+    	})
         Lists.remove({
-          name: "testowa",
+          name: "testowaD",
         });
-    var lista = Lists.find({ name: 'testowa'}).fetch();
-    emit('lista', lista);
+    var lista4 = Lists.find({ name: 'testowaD'}).fetch();
+    emit('lista4', lista4);
     });
 
-    client.once('lista', function(lista) {
-         assert.equal(lista.length, 1);
+    client.once('lista4', function(lista4) {
+         assert.equal(lista4.length, 0);
          done();
        });
   });
